4006679cc127fa47ad1ae6d5ec0a0437c0fa82c1
Yuxuan Dai <yxdai@smail.nju.edu.cn>
2025-03-25 14:51:26 +0800

    0.2.1->0.3.0, change Axum to Actix-web, update dependencies (#48)
    * 0.2.1->0.3.0, change Axum to Actix-web, update dependencies
    * Fix inconsistencies between path names in 'lib.rs' (/rules/xxx) and 'routes/rules.rs' (/rule/xxx), renaming both to "/rules/xxx"
    * Change web framework from axum to actix-web, allowing co-compilation with the rest of crates-pro
    * Update to Rust 2024 edition
    * Update README.md
    
    Signed-off-by: Yuxuan Dai <yxdai@smail.nju.edu.cn>
    
    * Fix 'zip' version issue
    * Lock 'zip' version to "=2.4.2" (instead of "2.5") to allow 'utoipa-swagger-ui' compilation
    
    Signed-off-by: Yuxuan Dai <yxdai@smail.nju.edu.cn>
    
    * Upload devcontainer config files
    
    Signed-off-by: Yuxuan Dai <yxdai@smail.nju.edu.cn>
    
    * Fixes for cargo clippy
    
    Signed-off-by: Yuxuan Dai <yxdai@smail.nju.edu.cn>
    
    ---------
    
    Signed-off-by: Yuxuan Dai <yxdai@smail.nju.edu.cn>

--- src/entity/models.rs
+++ src/entity/models.rs
@@ -7,7 +7,7 @@ use sea_orm::{entity::prelude::*, ActiveValue};
 #[derive(Parser, Debug)]
 #[command(
     author = "yjchen",
-    version = "0.1.0",
+    version = "0.3.0",
     about = "sensleaks-rs",
     long_about = "sensleaks: A tool to detect sensitive information in Git repository",
     after_help = "run 'cargo run --bin api' to get REST API.\nRepository: https://github.com/open-rust-initiative/sensleak-rs"
@@ -148,7 +148,7 @@ pub struct Rule {
     /// Regular expression used to detect secrets.
     pub regex: String,
 
-    /// Float representing the minimum shannon entropy a regex group must have to be considered a secret.
+    // /// Float representing the minimum shannon entropy a regex group must have to be considered a secret.
     // pub entropy: Option<f64>,
 
     /// Keywords are used for pre-regex check filtering. Rules that contain keywords will perform a quick string compare check to make sure the keyword(s) are in the content being scanned. Ideally these values should either be part of the idenitifer or unique strings specific to the rule's regex
@@ -237,7 +237,7 @@ pub enum Relation {}
 impl ActiveModelBehavior for ActiveModel {}
 
 /// Represents an item in the scanned output.
-#[derive(Debug, Serialize, Deserialize, Clone)]
+#[derive(Debug, Serialize, Deserialize, Clone, ToSchema)]
 pub struct Leak {
     /// The line containing the sensitive information.
     pub line: String,
